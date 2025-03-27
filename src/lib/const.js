db80f91dff98c45ef9efad147935fc362c615db8
Rahul Kanyal <golusingh954015@gmail.com>
2025-03-27 12:26:15 +0530

    some changes

--- src/lib/const.js
+++ src/lib/const.js
@@ -5,7 +5,7 @@ export const WORK_EXPERIENCE = [
       tech: "Worked on 5G testing and antenna analysis. Gained hands-on exposure to radars, reflectometers, and 5G NR antenna setups. Collaborated with teams working on Nokia-BSNL 5G labs.",
       duration: "January 2025 - Present",
       company: "Nokia ",
-      URL: "/DATACOM-CERTIFICATE.pdf",
+      // URL: "/DATACOM-CERTIFICATE.pdf",
     },
     {
       title: "DRDO (Intern)",
@@ -13,7 +13,7 @@ export const WORK_EXPERIENCE = [
       tech: "Worked on Surface Acoustic Wave (SAW) devices. Involved in testing and development of resonators and filters used in transceivers. Learned characterization and measurement of RF components.",
       duration: "June 2024 - July 2024",
       company: "DRDO",
-      URL: "/hewlettpackardcertificate.pdf",
+      // URL: "/hewlettpackardcertificate.pdf",
     },
     {
       title: "Electronics Hive",
@@ -21,7 +21,7 @@ export const WORK_EXPERIENCE = [
       tech: "Embedded Systems, PCB Designing – Proficient in embedded solutions, microcontroller integration, schematic design, PCB routing, and prototyping for reliable hardware.",
       duration: "September 2024 - Novemeber 2024",
       company: "Electronics Hive",
-      URL: "/Scyscanner.pdf",
+      // URL: "/Scyscanner.pdf",
     },
   ];
   
